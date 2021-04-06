ec4cb4694e577fd3a809f450ab4a74a59a631955
Néstor Morales Hernández <nmoraleshernandez@batvoice.com>
2021-04-06 16:42:52 +0100

    Transforms _version.py to str

--- medikit/_version.py
+++ medikit/_version.py
@@ -1,2 +1,2 @@
 import subprocess
-__version__ = subprocess.check_output(["git", "describe", '--abbrev=8', '--always', 'HEAD']).strip()
+__version__ = str(subprocess.check_output(["git", "describe", '--abbrev=8', '--always', 'HEAD']).strip())
